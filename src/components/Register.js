7904918524dc6f9294fb618853d79cf5d6ac6a8c
Shubham Shrivastava <128039947+Shubhhammmm@users.noreply.github.com>
2023-06-18 14:38:58 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,24 +6,45 @@ function Register() {
     const [password, setPassword] = useState("");
     const [error, setError] = useState(false);
 
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+    const handlePassChange = (e) => {
+        setPassword(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (email.length <= 0 || password.length <= 0) {
+            setError(true)
+        }
+        else {
+            localStorage.setItem('user', JSON.stringify({
+                email, password
+            }))
+            setError(false)
+            setEmail('')
+            setPassword('')
+        }
+    }
     return (
         <div className='register'>
             <h2>Register</h2>
-            <form className='sign-up-form'>
-                <p className='error-para'>"Email or password isn't entered!"</p>
+            <form className='sign-up-form' onSubmit={handleSubmit}>
+                {error ? <p className='error-para'>"Email or password isn't entered!"</p>
+                    : null}
                 <div className='email-div'>
-                    <label htmlFor="email">Email: </label>
-                    <input type="email" id="email" />
+                    <label htmlFor="email" >Email: </label>
+                    <input value={email} onChange={handleEmailChange} type="email" id="email"  />
                 </div>
                 <div className='password-div'>
                     <label htmlFor="password">Password: </label>
-                    <input type="password" id="password" />
+                    <input type="password" id="password" value={password} onChange={handlePassChange}  />
                 </div>
                 <button type="submit" className='register-btn'>Register</button>
             </form>
             <div>
                 Already have an account?
-                <Link><button className='login-link'>Login</button></Link>
+                <Link to="/login"><button className='login-link'>Login</button></Link>
             </div>
         </div>
     )
